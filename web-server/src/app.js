2054076c60af9a63b907e5196b4228128fc65c1a
Freddy Martinez <fmartinez@belatrixsf.com>
2020-06-11 21:38:24 -0500

    Including handlebars and replacing static html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,27 +1,49 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("hbs");
 
 const app = express();
 
+// Paths
 const publicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
+// Setup handlebars
 app.use(express.static(publicPath));
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.get("", (req, res) => {
-  res.send("Hello world");
+  res.render("index");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help page");
+  res.render("help", {
+    helpText: "How can I help you?"
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About");
+  res.render("about");
 });
 
 app.get("/weather", (req, res) => {
   res.send({
     forecast: "Its snowing"
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    errorMessage: "Help article not found"
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    errorMessage: "Page not found"
   });
 });
 
