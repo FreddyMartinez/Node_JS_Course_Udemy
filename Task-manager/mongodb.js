c5201ab28982b1db121b908a327c9f710dac345d
Freddy Martinez <fmartinez@belatrixsf.com>
2020-06-17 13:52:22 -0500

    done with section 10

---
+++ Task-manager/mongodb.js
@@ -0,0 +1,34 @@
+const { MongoClient } = require("mongodb");
+
+const connectionUrl = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionUrl,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database");
+    }
+    const db = client.db(databaseName);
+
+    db.collection("users").insertMany(
+      [
+        {
+          name: "María",
+          age: 22
+        },
+        {
+          name: "José",
+          age: 26
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Error: " + error);
+        }
+        console.log(result);
+      }
+    );
+  }
+);
