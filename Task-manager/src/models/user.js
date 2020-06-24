02cfabe91dae3b68ef907635002fb87bfaa02c29
Freddy Martinez <fmartinez@belatrixsf.com>
2020-06-23 19:55:46 -0500

    finishing section 14

--- Task-manager/src/models/user.js
+++ Task-manager/src/models/user.js
@@ -97,6 +97,8 @@ userSchema.methods.toJSON = function() {
 
   delete userObject.password;
   delete userObject.tokens;
+  delete userObject.avatar;
+
   return userObject;
 };
 
--- Task-manager/src/routes/user.js
+++ Task-manager/src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const multer = require("multer");
+const sharp = require("sharp");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 
@@ -90,7 +91,11 @@ router.post(
   auth,
   upload.single("avatar"),
   async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+      .resize(250, 250)
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send("image uploaded");
   },
@@ -104,6 +109,22 @@ router.delete("/users/me/avatar", auth, async (req, res) => {
   req.user.avatar = undefined;
   await req.user.save();
   res.send("image deleted");
+});
+
+// Get Avatar
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error("Image not found");
+    }
+
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
 module.exports = router;
