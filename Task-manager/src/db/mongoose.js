0121f5b9e10e5a9d8d0d5258cd94c6c23787034b
Freddy Martinez <fmartinez@belatrixsf.com>
2020-06-24 13:52:01 -0500

    quick fix - wrapping up section 15

--- Task-manager/src/db/mongoose.js
+++ Task-manager/src/db/mongoose.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectionUrl = process.env.MONGO_URL;
 
-const databaseName = "task-manager";
-
-mongoose.connect(connectionUrl + "/" + databaseName, {
+mongoose.connect(connectionUrl, {
   useNewUrlParser: true,
   useCreateIndex: true
 });
