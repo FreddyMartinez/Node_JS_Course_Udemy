9a6d3067aa5eaf1f3330d7debec2ffcbb74060eb
Freddy Martinez <fmartinez@belatrixsf.com>
2020-06-27 21:31:40 -0500

    implementing acknowledgements & using bad-words lib

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -10,5 +10,25 @@ messageForm.addEventListener("submit", e => {
 
   const message = e.target.elements.message.value;
 
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, response => {
+    console.log(response);
+  });
+});
+
+document.querySelector("#send-location-btn").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("geolocation not supported by this browser");
+  }
+  navigator.geolocation.getCurrentPosition(position => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },
+      response => {
+        console.log(response);
+      }
+    );
+  });
 });
