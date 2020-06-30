c81a8c0c4153573d13206e9ddbcb5d91de372ba7
Freddy Martinez <fmartinez@belatrixsf.com>
2020-06-29 22:08:43 -0500

    implementing users logic , creating sidebar & join

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,24 +1,64 @@
 const socket = io();
 
-socket.on("message", message => {
-  console.log(message);
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageInput = document.querySelector("input");
+const $messageButton = document.querySelector("#send-msg-btn");
+const $locationButton = document.querySelector("#send-location-btn");
+const $messages = document.querySelector("#messages");
+
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationTemplate = document.querySelector("#location-template").innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
+
+// Options
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
 });
 
-const messageForm = document.querySelector("#message-form");
-messageForm.addEventListener("submit", e => {
+socket.on("message", message => {
+  const newMsg = Mustache.render(messageTemplate, {
+    username: message.username,
+    message: message.text,
+    time: moment(message.createdAt).format("h:mm a")
+  });
+  $messages.insertAdjacentHTML("beforeend", newMsg);
+});
+
+socket.on("locationMessage", url => {
+  const location = Mustache.render(locationTemplate, {
+    username: url.username,
+    url: url.text,
+    time: moment(url.createdAt).format("h:mm a")
+  });
+  $messages.insertAdjacentHTML("beforeend", location);
+});
+
+$messageForm.addEventListener("submit", e => {
   e.preventDefault();
+
+  $messageButton.setAttribute("disabled", "disabled");
 
   const message = e.target.elements.message.value;
 
-  socket.emit("sendMessage", message, response => {
-    console.log(response);
+  socket.emit("sendMessage", message, error => {
+    $messageButton.removeAttribute("disabled");
+    $messageInput.value = "";
+    $messageInput.focus();
+    if (error) {
+      alert(error);
+    }
   });
 });
 
-document.querySelector("#send-location-btn").addEventListener("click", () => {
+$locationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("geolocation not supported by this browser");
   }
+
+  $locationButton.setAttribute("disabled", "disabled");
+
   navigator.geolocation.getCurrentPosition(position => {
     socket.emit(
       "sendLocation",
@@ -27,8 +67,24 @@ document.querySelector("#send-location-btn").addEventListener("click", () => {
         longitude: position.coords.longitude
       },
       response => {
+        $locationButton.removeAttribute("disabled");
         console.log(response);
       }
     );
   });
+});
+
+socket.emit("join", { username, room }, error => {
+  if (error) {
+    alert(error);
+    location = "/";
+  }
+});
+
+socket.on("roomData", ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users
+  });
+  document.querySelector("#sidebar").innerHTML = html;
 });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -3,7 +3,15 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const Filter = require("bad-words");
+const generateMessage = require("./utils/messages");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require("./utils/users");
 
+// Seting up server
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -13,31 +21,71 @@ const publicDirectoryPaht = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPaht));
 
+// Establishing connection
 io.on("connection", socket => {
-  socket.emit("message", "Welcome!");
-  socket.broadcast.emit("message", "A new user has joined");
+  // Someone joins a room
+  socket.on("join", ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room });
 
+    if (error) {
+      callback(error);
+    }
+
+    socket.join(room);
+
+    socket.emit("message", generateMessage("App", "Welcome!"));
+    socket.broadcast
+      .to(room)
+      .emit("message", generateMessage("App", `${username} has joined`));
+
+    io.to(room).emit("roomData", {
+      room,
+      users: getUsersInRoom(room)
+    });
+    callback();
+  });
+
+  // Someone send a message
   socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
     const filter = new Filter();
 
     if (filter.isProfane(message)) {
       return callback("Profanity is not allowed");
     }
 
-    io.emit("message", message);
-    callback("Message delivered");
+    io.to(user.room).emit("message", generateMessage(user.username, message));
+    callback();
   });
 
+  // Someone send ther location
   socket.on("sendLocation", (location, callback) => {
-    io.emit(
-      "message",
-      `Location-> lat: ${location.latitude}, lon: ${location.longitude}`
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
+      "locationMessage",
+      generateMessage(
+        user.username,
+        `https://www.google.com/maps/@${location.latitude},${location.longitude}`
+      )
     );
     callback("Location shared");
   });
 
+  // Someone is desconected
   socket.on("disconnect", () => {
-    io.emit("message", "Someone has left");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage("App", `${user.username} has left the room`)
+      );
+
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
+    }
   });
 });
 
